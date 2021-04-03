67b605fb84bc692797bbfbccd4493c52a1a61854
Steve Grieb <sgrieb64@gmail.com>
2021-04-03 18:43:45 -0400

    add at layer

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,6 +5,7 @@
 <script>
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import * as esri from 'esri-leaflet';
 
 export default {
   name: "LeafletMap",
@@ -14,10 +15,14 @@ export default {
     };
   },
   mounted() {
-    this.map = L.map("mapContainer").setView([51.959, -8.623], 12);
+    this.map = L.map("mapContainer").setView([45.526, -122.667], 12);
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    esri.featureLayer({
+      url: 'https://services1.arcgis.com/fBc8EJBxQRMcHlei/ArcGIS/rest/services/ANST_Facilities/FeatureServer/8'
     }).addTo(this.map);
   },
   beforeUnmount() {
