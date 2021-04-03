2438a36c0b03dc09e69b3f75bc73323dcdb13455
Steve Grieb <sgrieb64@gmail.com>
2021-04-03 11:56:21 -0400

    add  leaflet

---
+++ src/components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="mapContainer"></div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default {
+  name: "LeafletMap",
+  data() {
+    return {
+      map: null
+    };
+  },
+  mounted() {
+    this.map = L.map("mapContainer").setView([51.959, -8.623], 12);
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  },
+  beforeUnmount() {
+    if (this.map) {
+      this.map.remove();
+    }
+  }
+}
+</script>
+
+<style scoped>
+#mapContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
